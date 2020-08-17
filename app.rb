a11298ea1c74ea86d06d63c89b41dceb8283eaf3
kristyna cervenkova <kristyna275@gmail.com>
2020-08-17 17:16:43 +0200

    kitten

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+set :session_secret, 'super secret'
+
+get '/' do
+    'hello!'
+  end
+
+get '/secret' do
+    'message'
+end 
+
+get '/cat' do
+    "<div>
+   <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+   </div>"
+end 
